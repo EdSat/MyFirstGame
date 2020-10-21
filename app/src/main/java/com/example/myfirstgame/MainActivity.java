1ca29fb4ae7135f3c80604aea042512d7acde47e
EdSat <esatar@me.com>
2020-10-21 14:51:13 +0200

    Added a landing page for the game + play button without function

---
+++ app/src/main/java/com/example/myfirstgame/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfirstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Log.i("ImageButton", "Clicked");
+    }
+}
